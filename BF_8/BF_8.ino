fc5648da3a586b0d3e12ce87af524729dcf3255c
Erik Herrmann <eherrmann1991@gmail.com>
2025-11-11 17:12:56 -0600

    Complete refactor of the code to make it more maintainable. Also finally fixed the ADC flicker

--- BF_8/BF_8.ino
+++ BF_8/BF_8.ino
@@ -7,28 +7,60 @@
 
 // Pin definitions
 #define CD4051_INH 19    // CD4051 inhibit pin
-#define CD4051_A   9    // CD4051 control pin A
-#define CD4051_B   10   // CD4051 control pin B
-#define CD4051_C   15   // CD4051 control pin C
+#define CD4051_A   9     // CD4051 control pin A
+#define CD4051_B   10    // CD4051 control pin B
+#define CD4051_C   15    // CD4051 control pin C
 #define BUTTON_PINS {1, 2, 3, 4, 5, 6, 7, 8} // 8 buttons
 #define PROGRAM_BUTTON 0 // Programming button
-#define TM1637_CLK 16   // TM1637 CLK pin
-#define TM1637_DIO 14   // TM1637 DIO pin
-#define ANALOG_PIN A0   // CD4051 output to ADC
+#define TM1637_CLK 16    // TM1637 CLK pin
+#define TM1637_DIO 14    // TM1637 DIO pin
+#define ANALOG_PIN A0    // CD4051 output to ADC
 
 // MIDI settings
 #define MIDI_CHANNEL_DEFAULT 1  // Default MIDI channel
+#define MIDI_CHANNEL_MIN 1
+#define MIDI_CHANNEL_MAX 16
+#define MIDI_CC_MIN 1
+#define MIDI_CC_MAX 127
+#define MIDI_VALUE_MAX 127
+#define MIDI_BUTTON_ON 127
+#define MIDI_BUTTON_OFF 0
+
 const int CC_FADERS_DEFAULT[] = {1, 2, 3, 4, 5, 6, 7, 8}; // Default fader CCs
 const int CC_BUTTONS_DEFAULT[] = {9, 10, 11, 12, 13, 14, 15, 16}; // Default button CCs
 
 // Smoothing settings
 #define NUM_FADERS 8
 #define NUM_BUTTONS 8
-#define AVERAGE_SAMPLES 20 // Number of ADC readings to average
+#define AVERAGE_SAMPLES 30 // Number of ADC readings to average (increased for better smoothing)
+#define ADC_SAMPLES_PER_READ 5 // Number of ADC samples to take and average per fader read
+#define CD4051_SETTLE_TIME 1 // Microseconds to wait after switching CD4051 channel
+#define MIDI_DEADBAND 2 // Minimum MIDI value change required to update (hysteresis)
 #define SMOOTHING_DELAY 500
 
 // Program mode settings
-#define PROGRAM_TIMEOUT 4000 // ms to revert to operational mode (4s)
+#define PROGRAM_TIMEOUT 4000        // ms to revert to operational mode (4s)
+#define DISPLAY_TIMEOUT 1000        // ms to revert display after button press
+#define PROGRAM_MODE_OFF 0
+#define PROGRAM_MODE_FADER_CC 1
+#define PROGRAM_MODE_BUTTON_CC 2
+#define PROGRAM_MODE_CHANNEL 3
+#define PROGRAM_MODE_COUNT 4
+
+// EEPROM addresses
+#define EEPROM_ADDR_FADERS_START 0
+#define EEPROM_ADDR_BUTTONS_START NUM_FADERS
+#define EEPROM_ADDR_MIDI_CHANNEL (NUM_FADERS * 2)
+
+// ADC and mapping constants
+#define ADC_MAX 1023
+#define MIDI_MAP_MAX 128
+#define CHANNEL_MAP_MAX 17
+
+// Display settings
+#define DISPLAY_BRIGHTNESS 4
+#define BUTTON_DEBOUNCE_INTERVAL 5
+#define LOOP_DELAY 10
 
 // TM1637 display
 TM1637Display display(TM1637_CLK, TM1637_DIO);
@@ -84,15 +116,52 @@ long totals[NUM_FADERS] = {0, 0, 0, 0, 0, 0, 0, 0}; // Running totals
 unsigned long buttonPressTime = 0; // Track button press time
 int activeFader = 0; // Last-touched fader (0-7)
 bool showingFaderID = false; // Track if showing "F1"-"F8"
-unsigned long lastMessage; // Time that the last MIDI message was sent
+unsigned long lastMessage = 0; // Time that the last MIDI message was sent
 
 // Program mode variables
-int programMode = 0; // 0: off, 1: fader CC, 2: button CC, 3: channel
+int programMode = PROGRAM_MODE_OFF; // 0: off, 1: fader CC, 2: button CC, 3: channel
 unsigned long lastProgramActivity = 0; // Track last activity for timeout
 int programFader = -1; // Fader being programmed
 
+// Forward declarations
+void initializeHardware();
+void initializeReadings();
+void loadSettingsFromEEPROM();
+void handleProgramButton();
+void handleProgramModeTimeout();
+void processNormalMode();
+void processProgramMode();
+int readFaderValue(int faderIndex);
+int clampMidiValue(int value);
+int clampMidiChannel(int value);
+void selectCD4051Channel(int channel);
+void updateFaderDisplay(int faderIndex, int midiValue);
+void handleButtonPress(int buttonIndex, int buttonState);
+void saveProgramModeSettings();
+
 void setup() {
-   // Initialize pins
+  initializeHardware();
+  initializeReadings();
+  loadSettingsFromEEPROM();
+}
+
+void loop() {
+  programButton.update();
+  
+  handleProgramButton();
+  handleProgramModeTimeout();
+  
+  if (programMode == PROGRAM_MODE_OFF) {
+    processNormalMode();
+  } else {
+    processProgramMode();
+  }
+  
+  delay(LOOP_DELAY);
+}
+
+void initializeHardware() {
+  // Initialize CD4051 pins
   pinMode(CD4051_INH, OUTPUT);
   pinMode(CD4051_A, OUTPUT);
   pinMode(CD4051_B, OUTPUT);
@@ -103,174 +172,242 @@ void setup() {
   int buttonPins[] = BUTTON_PINS;
   for (int i = 0; i < NUM_BUTTONS; i++) {
     buttons[i].attach(buttonPins[i], INPUT);
-    buttons[i].interval(5); // 5ms debounce time
+    buttons[i].interval(BUTTON_DEBOUNCE_INTERVAL);
   }
 
   // Initialize program button
   programButton.attach(PROGRAM_BUTTON, INPUT);
-  programButton.interval(5);
+  programButton.interval(BUTTON_DEBOUNCE_INTERVAL);
 
   // Initialize TM1637 display
-  display.setBrightness(4); // 0-7, 7 being max
-  display.clear(); //Start from blank
+  display.setBrightness(DISPLAY_BRIGHTNESS);
+  display.clear();
+}
 
-  // Initialize readings arrays
+void initializeReadings() {
   for (int i = 0; i < NUM_FADERS; i++) {
     for (int j = 0; j < AVERAGE_SAMPLES; j++) {
       readings[i][j] = 0;
     }
   }
+}
 
-  // Load settings from EEPROM
+void loadSettingsFromEEPROM() {
   for (int i = 0; i < NUM_FADERS; i++) {
-    ccFaders[i] = EEPROM.read(i); //Fader address 0-7
-    ccButtons[i] = EEPROM.read(i + NUM_BUTTONS); //Button addresses 8-15
-    if (ccFaders[i] > 127 || ccFaders[i] < 1) ccFaders[i] = CC_FADERS_DEFAULT[i]; // Check if faders are within CC range, otherwise set them to default
-    if (ccButtons[i] > 127 || ccButtons[i] < 1) ccButtons[i] = CC_BUTTONS_DEFAULT[i]; //Check if buttons are within CC range, set to default other
-  }
-  midiChannel = EEPROM.read(NUM_FADERS * 2); //MidiChannel address 16
-  if (midiChannel < 1 || midiChannel > 16) midiChannel = MIDI_CHANNEL_DEFAULT; //Check if midiChannel is within channel range, otherwise set it to defualt
-}
-
-void loop() {
-  // Handle program button
-  programButton.update();
-  if (programButton.rose()) { //Only trigger button on rising edge
-    // Cycle through program modes (0 -> 1 -> 2 -> 3 -> 0)
-    programMode = (programMode + 1) % 4;
-    lastProgramActivity = millis(); // Update activity time
-    programFader = -1; // Reset programming fader
-    if (programMode == 1) display.setSegments(CCF); // Show "CCF"
-    else if (programMode == 2) display.setSegments(CCB); // Show "CCB"
-    else if (programMode == 3) display.setSegments(CH); // Show "CH"
-    else {
-      display.clear();
-      updateDisplay(lastFaderValues[activeFader]); // Restore fader value
+    ccFaders[i] = EEPROM.read(EEPROM_ADDR_FADERS_START + i);
+    ccButtons[i] = EEPROM.read(EEPROM_ADDR_BUTTONS_START + i);
+    
+    if (ccFaders[i] > MIDI_CC_MAX || ccFaders[i] < MIDI_CC_MIN) {
+      ccFaders[i] = CC_FADERS_DEFAULT[i];
+    }
+    if (ccButtons[i] > MIDI_CC_MAX || ccButtons[i] < MIDI_CC_MIN) {
+      ccButtons[i] = CC_BUTTONS_DEFAULT[i];
     }
   }
+  
+  midiChannel = EEPROM.read(EEPROM_ADDR_MIDI_CHANNEL);
+  if (midiChannel < MIDI_CHANNEL_MIN || midiChannel > MIDI_CHANNEL_MAX) {
+    midiChannel = MIDI_CHANNEL_DEFAULT;
+  }
+}
 
-  // Check for program mode timeout
-  if (programMode != 0 && millis() - lastProgramActivity > PROGRAM_TIMEOUT) { // Check if we're in a program mode and the timeout period has elapsed
-      // Save settings if changes were made
-      if (programMode == 1) {
-        EEPROM.update(programFader, ccFaders[programFader]);
-      } else if (programMode == 2) {
-        EEPROM.update(programFader + NUM_FADERS, ccButtons[programFader]);
-      } else if (programMode == 3) {
-        EEPROM.update(NUM_FADERS * 2, midiChannel);
-      }
-    programMode = 0; // Exit program mode
+void handleProgramButton() {
+  if (programButton.rose()) {
+    programMode = (programMode + 1) % PROGRAM_MODE_COUNT;
+    lastProgramActivity = millis();
+    programFader = -1;
+    
+    switch (programMode) {
+      case PROGRAM_MODE_FADER_CC:
+        display.setSegments(CCF);
+        break;
+      case PROGRAM_MODE_BUTTON_CC:
+        display.setSegments(CCB);
+        break;
+      case PROGRAM_MODE_CHANNEL:
+        display.setSegments(CH);
+        break;
+      default:
+        display.clear();
+        updateDisplay(lastFaderValues[activeFader]);
+        break;
+    }
+  }
+}
+
+void handleProgramModeTimeout() {
+  if (programMode != PROGRAM_MODE_OFF && 
+      millis() - lastProgramActivity > PROGRAM_TIMEOUT) {
+    saveProgramModeSettings();
+    programMode = PROGRAM_MODE_OFF;
     display.clear();
-    updateDisplay(lastFaderValues[activeFader]); // Restore fader value
+    updateDisplay(lastFaderValues[activeFader]);
   }
-
-  // Normal mode: Read faders and buttons
-  if (!programMode) {
-    // Read faders via CD4051
-    for (int i = 0; i < NUM_FADERS; i++) { //loop over each fader in decimal
-      digitalWrite(CD4051_A, i & 0x01); // this line and the next two convert i from decimal to binary and properly address the CD4051 to the channel we want
-      digitalWrite(CD4051_B, (i >> 1) & 0x01);
-      digitalWrite(CD4051_C, (i >> 2) & 0x01);
-      //Original filter
-      int analogValue = analogRead(ANALOG_PIN);//read the analog pin
-      totals[i] -= readings[i][readIndices[i]]; // remove the value at the fader index for averaging
-      readings[i][readIndices[i]] = analogValue; // insert the current analog value into the averaging array
-      totals[i] += analogValue; //add the new value to the totals
-      readIndices[i] = (readIndices[i] + 1) % AVERAGE_SAMPLES;
-      int average = totals[i] / AVERAGE_SAMPLES;
-      int midiValue = map(average, 0, 1023, 0, 128);
-
-      if ((midiValue != lastFaderValues[i] && millis() - lastMessage < SMOOTHING_DELAY) || (abs(midiValue - lastFaderValues[i]) > 1 && millis() - lastMessage > SMOOTHING_DELAY)) {
-        midiValue == 128 ? midiValue = 127 : midiValue = midiValue;
-        sendMIDI(ccFaders[i], midiValue); // Send MIDI if fader value has changed
-        lastMessage = millis();
-        if (!showingFaderID && activeFader == i) {
-          updateDisplay(midiValue);
-        }
-        lastFaderValues[i] = midiValue;
-        activeFader = i;
-      }
-    }
-
-    // Read buttons
-    for (int i = 0; i < NUM_BUTTONS; i++) {
-      buttons[i].update(); // Update each button state
-      int buttonState = buttons[i].read(); // Read that state
-      if (buttonState != lastButtonStates[i]) { // Compare to last state
-        if (buttonState == HIGH) { // If state changed from LOW to HIGH (this could also be done with buttons[i].rose() )
-          sendMIDI(ccButtons[i], 127); // Send button CC with value 127
-          display.setSegments(FADER_IDS[i], 4, 0); // Display fader button ID
-          showingFaderID = true;
-          buttonPressTime = millis(); // log the time the display changed for timeout purposes below
-          activeFader = i;
-        } else {
-          sendMIDI(ccButtons[i], 0); // Send button CC with value 0
-          display.setSegments(FADER_IDS[i], 4, 0); // Display fader button ID
-          showingFaderID = true;
-          buttonPressTime = millis(); // log the time the display changed for timeout purposes below
-          activeFader = i;
-          }
-        lastButtonStates[i] = buttonState;
-      }
-    }
-
-    // Revert to fader value after 1 second
-    if (showingFaderID && millis() - buttonPressTime > 1000) {
-      display.clear();
-      updateDisplay(lastFaderValues[activeFader]);
-      showingFaderID = false;
-    }
-  } else {
-    // Program mode: Read faders to set CCs or channel
-    for (int i = 0; i < NUM_FADERS; i++) { //Same logic as above for operation mode. Consider converting this section to a function to avoid writing twice.
-      digitalWrite(CD4051_A, i & 0x01);
-      digitalWrite(CD4051_B, (i >> 1) & 0x01);
-      digitalWrite(CD4051_C, (i >> 2) & 0x01);
-      int analogValue = analogRead(ANALOG_PIN);
-      totals[i] -= readings[i][readIndices[i]];
-      readings[i][readIndices[i]] = analogValue;
-      totals[i] += analogValue;
-      readIndices[i] = (readIndices[i] + 1) % AVERAGE_SAMPLES;
-      int average = totals[i] / AVERAGE_SAMPLES;
-      int value = map(average, 0, 1023, 0, 128);
-      value == 128 ? value = 127 : value = value;
-
-      if (value != lastFaderValues[i]) {
-        if (programMode == 1) {
-          ccFaders[i] = value;
-          programFader = i;
-          display.showNumberDec(value, false, 3, 1); // Show CC value
-        } else if (programMode == 2) {
-          ccButtons[i] = value;
-          programFader = i;
-          display.showNumberDec(value, false, 3, 1); // Show CC value
-        } else if (programMode == 3) {
-          value =  map(average, 0, 1023, 1, 17);
-          value == 17 ? value = 16 : value = value; 
-          midiChannel = value;
-          programFader = i;
-          display.showNumberDec(value, true, 2, 2); // Show channel (01-16)
-          value = map(average, 0, 1023, 0, 128);
-          value == 128 ? value = 127 : value = value;
-        }
-        lastFaderValues[i] = value;
-        lastProgramActivity = millis(); // Update activity time
-      }
-    }
-  }
-
-  // Small delay to avoid overloading MIDI
-  delay(10);
 }
 
-// Send MIDI CC message
+void saveProgramModeSettings() {
+  if (programFader >= 0 && programFader < NUM_FADERS) {
+    switch (programMode) {
+      case PROGRAM_MODE_FADER_CC:
+        EEPROM.update(EEPROM_ADDR_FADERS_START + programFader, ccFaders[programFader]);
+        break;
+      case PROGRAM_MODE_BUTTON_CC:
+        EEPROM.update(EEPROM_ADDR_BUTTONS_START + programFader, ccButtons[programFader]);
+        break;
+      case PROGRAM_MODE_CHANNEL:
+        EEPROM.update(EEPROM_ADDR_MIDI_CHANNEL, midiChannel);
+        break;
+    }
+  }
+}
+
+void processNormalMode() {
+  // Read and process faders
+  for (int i = 0; i < NUM_FADERS; i++) {
+    int midiValue = readFaderValue(i);
+    midiValue = clampMidiValue(midiValue);
+    
+    // Calculate change from last value
+    int valueChange = abs(midiValue - lastFaderValues[i]);
+    
+    // Apply deadband/hysteresis: only update if change is significant enough
+    // This prevents flickering when ADC oscillates between adjacent values
+    bool shouldSend = false;
+    if (lastFaderValues[i] == -1) {
+      // First read - always send
+      shouldSend = true;
+    } else if (millis() - lastMessage < SMOOTHING_DELAY) {
+      // Within smoothing delay - send if any change detected
+      shouldSend = (valueChange > 0);
+    } else {
+      // Outside smoothing delay - require deadband threshold
+      shouldSend = (valueChange >= MIDI_DEADBAND);
+    }
+    
+    if (shouldSend) {
+      sendMIDI(ccFaders[i], midiValue);
+      lastMessage = millis();
+      updateFaderDisplay(i, midiValue);
+      lastFaderValues[i] = midiValue;
+      activeFader = i;
+    }
+  }
+
+  // Read and process buttons
+  for (int i = 0; i < NUM_BUTTONS; i++) {
+    buttons[i].update();
+    int buttonState = buttons[i].read();
+    if (buttonState != lastButtonStates[i]) {
+      handleButtonPress(i, buttonState);
+      lastButtonStates[i] = buttonState;
+    }
+  }
+
+  // Revert to fader value after timeout
+  if (showingFaderID && millis() - buttonPressTime > DISPLAY_TIMEOUT) {
+    display.clear();
+    updateDisplay(lastFaderValues[activeFader]);
+    showingFaderID = false;
+  }
+}
+
+void processProgramMode() {
+  for (int i = 0; i < NUM_FADERS; i++) {
+    int value = readFaderValue(i);
+    value = clampMidiValue(value);
+    
+    if (value != lastFaderValues[i]) {
+      if (programMode == PROGRAM_MODE_FADER_CC) {
+        ccFaders[i] = value;
+        programFader = i;
+        display.showNumberDec(value, false, 3, 1);
+      } else if (programMode == PROGRAM_MODE_BUTTON_CC) {
+        ccButtons[i] = value;
+        programFader = i;
+        display.showNumberDec(value, false, 3, 1);
+      } else if (programMode == PROGRAM_MODE_CHANNEL) {
+        int average = totals[i] / AVERAGE_SAMPLES;
+        value = map(average, 0, ADC_MAX, MIDI_CHANNEL_MIN, CHANNEL_MAP_MAX);
+        value = clampMidiChannel(value);
+        midiChannel = value;
+        programFader = i;
+        display.showNumberDec(value, true, 2, 2);
+        // Restore value for lastFaderValues tracking
+        value = readFaderValue(i);
+        value = clampMidiValue(value);
+      }
+      lastFaderValues[i] = value;
+      lastProgramActivity = millis();
+    }
+  }
+}
+
+int readFaderValue(int faderIndex) {
+  selectCD4051Channel(faderIndex);
+  delayMicroseconds(CD4051_SETTLE_TIME); // Allow signal to settle after channel switch
+  
+  // Take multiple ADC samples and average them to reduce noise
+  long sampleSum = 0;
+  for (int i = 0; i < ADC_SAMPLES_PER_READ; i++) {
+    sampleSum += analogRead(ANALOG_PIN);
+  }
+  int analogValue = sampleSum / ADC_SAMPLES_PER_READ;
+  
+  // Update moving average
+  totals[faderIndex] -= readings[faderIndex][readIndices[faderIndex]];
+  readings[faderIndex][readIndices[faderIndex]] = analogValue;
+  totals[faderIndex] += analogValue;
+  readIndices[faderIndex] = (readIndices[faderIndex] + 1) % AVERAGE_SAMPLES;
+  
+  int average = totals[faderIndex] / AVERAGE_SAMPLES;
+  return map(average, 0, ADC_MAX, 0, MIDI_MAP_MAX);
+}
+
+void selectCD4051Channel(int channel) {
+  digitalWrite(CD4051_A, channel & 0x01);
+  digitalWrite(CD4051_B, (channel >> 1) & 0x01);
+  digitalWrite(CD4051_C, (channel >> 2) & 0x01);
+}
+
+int clampMidiValue(int value) {
+  if (value == MIDI_MAP_MAX) {
+    return MIDI_VALUE_MAX;
+  }
+  return value;
+}
+
+int clampMidiChannel(int value) {
+  if (value == CHANNEL_MAP_MAX) {
+    return MIDI_CHANNEL_MAX;
+  }
+  return value;
+}
+
+void updateFaderDisplay(int faderIndex, int midiValue) {
+  if (!showingFaderID && activeFader == faderIndex) {
+    updateDisplay(midiValue);
+  }
+}
+
+void handleButtonPress(int buttonIndex, int buttonState) {
+  if (buttonState == HIGH) {
+    sendMIDI(ccButtons[buttonIndex], MIDI_BUTTON_ON);
+  } else {
+    sendMIDI(ccButtons[buttonIndex], MIDI_BUTTON_OFF);
+  }
+  
+  display.setSegments(FADER_IDS[buttonIndex], 4, 0);
+  showingFaderID = true;
+  buttonPressTime = millis();
+  activeFader = buttonIndex;
+}
+
 void sendMIDI(byte cc, byte value) {
-  midiEventPacket_t midiPacket = {0x0B, 0xB0 | (midiChannel - 1), cc, value}; // Header (cable + command), byte1 (command + channel), cc, value
+  midiEventPacket_t midiPacket = {0x0B, 0xB0 | (midiChannel - 1), cc, value};
   MidiUSB.sendMIDI(midiPacket);
   MidiUSB.flush();
 }
 
-// Update TM1637 display with MIDI value (0-127)
 void updateDisplay(int value) {
-  display.showNumberDec(value, false, 3, 1); // Value, leading zeroes, length, starting position from left
+  display.showNumberDec(value, false, 3, 1);
 }
